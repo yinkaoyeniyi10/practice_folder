a5ee4df2b23582ed31ae92f4a135827b5d858084
Oyeyinka Oyeniyi <yinkaoyeniyi10@gmail.com>
2026-07-01 17:56:31 -0500

    Slight updates

--- app/__init__.py
+++ app/__init__.py
@@ -37,4 +37,4 @@ if not app.debug:
     app.logger.addHandler(file_handler)
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
-from app import routes, models, errors
+from app import routes, models, errors, email
