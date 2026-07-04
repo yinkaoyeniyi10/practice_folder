d8728b7615e91cb79e323abd900c2df8c5b77ce5
Oyeyinka Oyeniyi <yinkaoyeniyi10@gmail.com>
2026-07-04 14:26:30 -0500

    Added email functions

--- app/models.py
+++ app/models.py
@@ -7,6 +7,9 @@ from typing import Optional
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
+from time import time
+import jwt
+from app import app
 @login.user_loader
 def load_user(id):
     return db.session.get(User, int(id))
@@ -43,6 +46,18 @@ class User(UserMixin, db.Model):
                                                             primaryjoin=(followers.c.followed_id == id),
                                                             secondaryjoin=(followers.c.follower_id == id),
                                                             back_populates='following')
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256'
+        )
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return db.session.get(User, id)
     def follow(self, user):
         if not self.is_following(user):
             self.following.add(user)
