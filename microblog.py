f76fb70364b836358c2a4fb8fc43b482c748d96e
Yinka <yinkaoyeniyi10@gmail.com>
2026-08-28 19:45:14 -0500

    Made new dockerfile. REMINDER: requirements.txt needs to be redone so that it will work on other computers

--- microblog.py
+++ microblog.py
@@ -2,8 +2,10 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import app, db
 from app.models import User, Post
+import os
 @app.shell_context_processor
 def make_shell_context():
     return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
